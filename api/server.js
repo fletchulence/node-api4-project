f9cff330d1240f4332c479447ad6bc6181a6e501
david fletcher <davidfletcheraaron@gmail.com>
2022-01-13 17:20:41 -0500

    cors added

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 
 const server = express()
 
 server.use(express.json())
+server.use(cors())
 
 server.get('/hello', (req, res)=>{
    res.json({ message: 'hello my friends -- this will probably end up beingn my backend for my portfolio webpage' })
