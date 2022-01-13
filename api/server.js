7897cad1fe7f850b8bca7ff2a01838d451934436
david fletcher <davidfletcheraaron@gmail.com>
2022-01-13 17:08:15 -0500

    tested and running

--- api/server.js
+++ api/server.js
@@ -2,8 +2,16 @@ const express = require('express')
 
 const server = express()
 
+server.use(express.json())
 
+server.get('/hello', (req, res)=>{
+   res.json({ message: 'hello my friends -- this will probably end up beingn my backend for my portfolio webpage' })
+})
 
-
+server.get('*', (req, res)=>{
+   res.send(`
+      <h1> My App is Up! </h1>
+   `)
+})
 
 module.exports = server;
