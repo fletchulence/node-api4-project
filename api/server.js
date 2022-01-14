4390f07f153b646bc0df4fa230d14cfb765f875e
david fletcher <davidfletcheraaron@gmail.com>
2022-01-14 16:29:58 -0500

    looks like registering a new user is wokring

--- api/server.js
+++ api/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const cors = require('cors')
 
-const users = process.env
+let users = [
+   {username: 'davidfletcher', firstName: 'david', password: 'david1234' }, 
+   {username: 'sarafletcher', firstName: 'sara', password: 'sara1234' }
+]
 
 const server = express()
 
@@ -12,22 +15,36 @@ server.get('/hello', (req, res)=>{
    res.json({ message: 'hello my friends -- this will probably end up beingn my backend for my portfolio webpage' })
 })
 
+// [GET] gets all users
 server.get('/api/users', (req, res, next)=>{
-   res.json([
-      {username: 'davidfletcher', firstName: 'david', password: 'david1234' }, 
-      {username: 'sarafletcher', firstName: 'sara', password: 'sara1234' }
-   ])
+   res.json(users)
 })
 
-
-server.get('/api/register', (req, res, next)=>{
-   console.log(users)
+// [POST] posts a new registrant to the state
+server.post('/api/register',  (req, res, next)=>{
+   // console.log(users)
+   const {firstName, password, username} = req.body
+   try{
+      users.push({ ...req.body, username, firstName, password }) 
+      return res.status(201).json( users )
+   } catch(err){
+      next(err)
+   }
 })
+
+//
 
 server.get('*', (req, res)=>{
    res.send(`
       <h1> My App is Up! </h1>
    `)
+})
+
+server.use((err, req, res, next)=>{
+   res.status(err.status || 500).json({
+      message: err.message,
+      stack: err.stack
+   })
 })
 
 module.exports = server;
