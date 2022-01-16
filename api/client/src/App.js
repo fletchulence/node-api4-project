d9c82f7d031476960d060b1de481abf1efd4cc69
david fletcher <davidfletcheraaron@gmail.com>
2022-01-16 16:08:26 -0500

    this is now working to how i want!!

--- api/client/src/App.js
+++ api/client/src/App.js
@@ -1,24 +1,43 @@
+import {useEffect, useState} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [response, setResponse] = useState([]);
+  
+  useEffect(() => {
+    fetch('/hello')
+      .then(res => res.json())
+      .then(d => setResponse(d.message))
+      // console.log(data)
+    }, [])
+    console.log(response)
+    
+    
   return (
+    // <React.Fragment>
+
+    // <div><p> what the heck</p>something here</div>
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/this.js</code> asdfasdf.
-        </p>
+        <div>
+          {response}
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Learn React
         </a>
       </header>
     </div>
+          // {/* </React.Fragment> */}
   );
 }
 
